51a79180d2b287d0802541ad4968fd86499e08a6
Adam Michalak <adam.michalak.dev@gmail.com>
2020-09-06 17:00:56 +0200

    add unit support

--- src/app/custom-style.directive.spec.ts
+++ src/app/custom-style.directive.spec.ts
@@ -7,13 +7,14 @@ import { By } from '@angular/platform-browser';
   template: `
     <div>No directive used</div>
     <div [customStyle]="{'background-color': 'red', 'color': 'green'}">Correct use</div>
+    <h1 [customStyle]="{'font-size.px': '18', 'min-height.vh': '20'}">With units</h1>
   `
 })
 class TestComponent {}
 
 describe('CustomStyleDirective', () => {
   let fixture: ComponentFixture<TestComponent>;
-  let elementsWithDirective: DebugElement;
+  let elementsWithDirective: DebugElement[];
   let elementWithoutDirective: DebugElement;
 
   beforeEach(() => {
@@ -25,7 +26,7 @@ describe('CustomStyleDirective', () => {
     }).createComponent(TestComponent);
     fixture.detectChanges(); // initial binding 
 
-    elementsWithDirective = fixture.debugElement.query(By.directive(CustomStyleDirective));
+    elementsWithDirective = fixture.debugElement.queryAll(By.directive(CustomStyleDirective));
     elementWithoutDirective = fixture.debugElement.query(By.css('div:not([customStyle])'));
   });
 
@@ -34,7 +35,12 @@ describe('CustomStyleDirective', () => {
   });
 
   it('should change style of an element with directive', () => {
-    expect(elementsWithDirective.styles.backgroundColor).toBe('red');
-    expect(elementsWithDirective.styles.color).toBe('green');
+    expect(elementsWithDirective[0].styles.backgroundColor).toBe('red');
+    expect(elementsWithDirective[0].styles.backgroundColor).toBe('red');
   });
+
+  it('should change style when unit is used', () => {
+    expect(elementsWithDirective[1].styles.fontSize).toBe('18px');
+    expect(elementsWithDirective[1].styles.minHeight).toBe('20vh');
+  })
 });
--- src/app/custom-style.directive.ts
+++ src/app/custom-style.directive.ts
@@ -16,16 +16,23 @@ export class CustomStyleDirective implements AfterViewInit {
   }
   
   private applyStyles() {
-    Object.keys(this.style).forEach(key => {
-      const keyInCamelCase = this.toCamelCase(key); 
-      if (this.el.nativeElement.style[keyInCamelCase] === undefined) {
-        throw new Error(`Property ${key} does not exist on element's style object.`);
+    Object.keys(this.style).forEach(keyWithUnit => {
+      const formattedKey = this.getFormattedKeyWithoutUnit(keyWithUnit);
+      const unit = this.isUnitPresent(keyWithUnit) ? this.getUnit(keyWithUnit) : '';
+
+      if (this.el.nativeElement.style[formattedKey] === undefined) {
+        throw new Error(`Property ${formattedKey} does not exist on element's style object.`);
       }
-      if (!this.style[key]) {
-        throw new Error(`Property ${key} has no value.`);
+      if (!this.style[keyWithUnit]) {
+        throw new Error(`Property ${keyWithUnit} has no value.`);
       }
-      this.el.nativeElement.style[keyInCamelCase] = this.style[key];
+
+      this.el.nativeElement.style[formattedKey] = this.style[keyWithUnit] + unit;
     });
+  }
+
+  private getFormattedKeyWithoutUnit(keyWithUnit: string): string {
+    return this.toCamelCase(keyWithUnit.split('.')[0].trim());
   }
   
   private toCamelCase(key: string): string {
@@ -34,5 +41,13 @@ export class CustomStyleDirective implements AfterViewInit {
       .replace(/-(.)/g, (match, firstLetterAfterDash) => {
         return firstLetterAfterDash.toUpperCase();
       });
+  }
+
+  private isUnitPresent(keyWithUnit: string): boolean {
+    return keyWithUnit.includes('.');
+  }
+
+  private getUnit(keyWithUnit: string): string {
+    return keyWithUnit.split('.')[1].trim();
   }
 }
